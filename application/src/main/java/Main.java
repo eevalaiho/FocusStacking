8c4e91e1ddb5859349c0fce7ba9790620619e877
Eeva-Maria Laiho <eeva-maria@laiho.net>
2019-06-05 19:16:58 +0300

    Another images

--- application/src/main/java/Main.java
+++ application/src/main/java/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Started");
 
-        String[] paths = new String[] {"150x100-koralli-mirrored-top-blur.png", "150x100-koralli-mirrored-left-blur.png", "150x100-koralli-mirrored-right-blur.png"};
-        int windowSize = 32;
+        //String[] paths = new String[] {"150x100-koralli-mirrored-top-blur.png", "150x100-koralli-mirrored-left-blur.png", "150x100-koralli-mirrored-right-blur.png"};
+        String[] paths = new String[] {"150x100-koralli2-mirrored-top-blur.png", "150x100-koralli2-mirrored-left-blur.png", "150x100-koralli2-mirrored-right-blur.png"};
+        int windowSize = 16;
 
         FocusStacking fstack = new FocusStacking(windowSize);
         try {
