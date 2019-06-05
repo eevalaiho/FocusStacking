9046ab465dbc7601f55dc6dcc2eb194fd6162bff
Eeva-Maria Laiho <eeva-maria@laiho.net>
2019-06-05 18:52:10 +0300

    Output 16

--- application/src/main/java/Main.java
+++ application/src/main/java/Main.java
@@ -6,10 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Started");
 
-        //String[] paths = new String[] {"150x100-Humming-bird-top-blur.png", "150x100-Humming-bird-left-blur.png", "150x100-Humming-bird-right-blur.png"};
-        String[] paths = new String[] {"90x60-Humming-bird-top-blur.png", "90x60-Humming-bird-left-blur.png", "90x60-Humming-bird-right-blur.png"};
-        //String[] paths = new String[] {"150x100-double-mirrored-humming-bird-top-blur.png", "150x100-double-mirrored-humming-bird-left-blur.png", "150x100-double-mirrored-humming-bird-right-blur.png"};
-        int windowSize = 16;
+        String[] paths = new String[] {"150x100-koralli-mirrored-top-blur.png", "150x100-koralli-mirrored-left-blur.png", "150x100-koralli-mirrored-right-blur.png"};
+        int windowSize = 32;
 
         FocusStacking fstack = new FocusStacking(windowSize);
         try {
